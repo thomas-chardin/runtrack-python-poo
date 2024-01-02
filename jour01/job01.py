da89af75d045dcc28c40492df4e959abf2a1ac38
thomas-chardin <thomas.chardin@laplateforme.io>
2024-01-02 10:59:57 +0100

    job 1 et 2 fait

---
+++ jour01/job01.py
@@ -0,0 +1,6 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 12
+        self.nombre2 = 3
+operation = Operation()
+print(operation)
---
+++ jour01/job02.py
@@ -0,0 +1,9 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 12
+        self.nombre2 = 3
+operation = Operation()
+print(f"le nombre1 est {operation.nombre1}" )
+print(f"le nombre1 est {operation.nombre2}" )
+print(operation)
+
