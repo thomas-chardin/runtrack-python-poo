2a0a5c5cbd12adfd0ea2340f80b9f9464fee7911
thomas-chardin <thomas.chardin@laplateforme.io>
2024-01-02 14:22:29 +0100

    job fait

---
+++ jour01/job03.py
@@ -0,0 +1,8 @@
+class Operation:
+    def __init__(self):
+        self.nombre1 = 12
+        self.nombre2 = 3
+    def addition(self):
+       print( self.nombre1 + self.nombre2 )
+operation = Operation()
+operation.addition()
