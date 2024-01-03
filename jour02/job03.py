36387b4fc86537f393454eea695beee7f9b9300a
thomas-chardin <thomas.chardin@laplateforme.io>
2024-01-03 13:42:48 +0100

    job03 fait

--- jour02/job03.py
+++ jour02/job03.py
@@ -3,6 +3,7 @@ class Livre():
         self.__titre = titre
         self.__auteur = auteur
         self.__pages = pages
+        self.__disponible = True
     def modifierTitre(self, titre):
         self.titre = titre
     def modifierAuteur(self, auteur):
@@ -18,8 +19,24 @@ class Livre():
         print(f"Le nom de l'auteur est {self.auteur}")
     def afficherPages(self):
         print(f"Le nombre de pages de ce livre est de {self.pages}")
+    def vérification(self):
+        if self.__disponible == True:
+            return True
+        else :
+            return False
+    def emprunter(self):
+        if self.vérification() == True:
+            self.__disponible = False
+        else : 
+            print("Ce livre est deja emprunter")
+    def rendre(self):
+        if self.vérification() == False:
+            self.__disponible = True
+        else : 
+            print("Ce livre est deja disponible") 
 livre = Livre("test", "thomas", 1)
 livre.modifierPages(8)
 livre.afficherPages()
 livre.modifierPages(-10)
 livre.modifierPages(1.6)
+
